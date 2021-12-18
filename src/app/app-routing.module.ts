d9f7f4a068eb2a61240934f938d0e6072713f15e
DAVM280599 <danivarm99@gmail.com>
2021-12-17 20:41:53 -0500

    angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { GaleriaComponent } from './components/galeria/galeria.component';
+import { HojaDeVidaComponent } from './components/hoja-de-vida/hoja-de-vida.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: GaleriaComponent},
+  { path: 'galeria', component: GaleriaComponent},
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'curriculum', component: HojaDeVidaComponent },
+  { path: '404', component: NotFoundComponent},
+  { path: '**', redirectTo: '404', pathMatch: 'full'}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GaleriaComponent } from './components/galeria/galeria.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { BarraNavegacionComponent } from './components/barra-navegacion/barra-navegacion.component';
+import { HojaDeVidaComponent } from './components/hoja-de-vida/hoja-de-vida.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GaleriaComponent,
+    ContactoComponent,
+    BarraNavegacionComponent,
+    HojaDeVidaComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
